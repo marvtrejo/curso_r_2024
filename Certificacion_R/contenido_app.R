d07a2288e3dd99206eb1121466139266d0e5fad5
marvtrejo <40899453+marvtrejo@users.noreply.github.com>
2024-08-23 15:02:56 -0600

    Actualizacion 23.08.24

--- Certificacion_R/contenido_app.R
+++ Certificacion_R/contenido_app.R
@@ -1,37 +1,502 @@
-# Tarjetas de datos generales ####
-id_cards <- list(
-  card(
-    full_screen = TRUE,
-    card_header("Descripción"),
-    textOutput("descripcion")
+# Base de datos ####
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(bslib)
+
+setwd("C:/Users/mitrejo/Mi unidad/SPPC/Github/curso_r_2024/Certificacion_R")
+
+inv_fed <- read_excel("inv_fed.xlsx", sheet = "inv_fed") %>% 
+  mutate(
+    modcve = paste0(mod, "-", cve),
+    ciclo  = factor(ciclo),
+    pp_cantidad = as.numeric(pp_cantidad),
+    po_cantidad = as.numeric(po_cantidad),
+    pa_cantidad = as.numeric(pa_cantidad),
+    p_aprobado = as.numeric(p_aprobado),
+    p_ejercido = as.numeric(p_ejercido)
+  )
+  
+evaluaciones <- read_excel("inv_fed.xlsx", sheet = "evaluaciones")  
+
+evolucion <- read_excel("inv_fed.xlsx", sheet = "evolucion") 
+
+## Funciones ####
+anio_sel = 2022
+ramos_disp <- function(anio_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel) %>% 
+    pull(ramo) %>% 
+    unique() %>% 
+    sort()
+}
+
+pp_disp <- function(anio_sel, ramo_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel) %>% 
+    pull(nombre) %>% 
+    unique() %>% 
+    sort()
+}
+
+# UI ####
+## Datos generales ####
+datosgrales_card <- card(
+  card_header(
+    class = "bg-dark",
+    "Datos generales"),
+  height = NULL,
+  full_screen = FALSE,
+  accordion_panel(
+    title = "Descripción",
+    textOutput("descr_ac")
   ),
-  card(
-    full_screen = TRUE,
-    card_header("Normatividad"),
-    textOutput("normatividad")
+  accordion_panel(
+    title = "Derecho social asociado",
+    textOutput("derecho_ac")
   ),
-  card(
-    full_screen = TRUE,
-    card_header("Apoyos que entrega"),
-    textOutput("apoyos")
+  accordion_panel(
+    title = "Normatividad principal",
+    textOutput("Normatividad y link")
+  ),
+  accordion_panel(
+    title = "Matriz de Indicadores para Resultados",
+    tableOutput("tabla_mir")
+  ),
+  accordion_panel(
+    title = "Evaluaciones realizadas al programa",
+    tableOutput("tabla_eval")
   )
 )
 
-# Value boxes de datos generales ####
-id_vb<- list(
-  value_box(
-    title = "Año de inicio", 
-    value = "2024", 
-    showcase = bsicons::bs_icon("calendar-event")
+## Poblaciones ####
+poblacion_card <- navset_card_tab(
+  full_screen = T,
+  title = "Poblaciones",
+  nav_panel(
+    "Cuantificación",
+    card_title("Evolución de las poblaciones del programa"), 
+    plotOutput("graf_pob")
   ),
-  value_box(
-    title = "Porcentaje del presupuesto de desarrollo social", 
-    value = scales::unit_format(unit = "%")(0.3), 
-    showcase = bsicons::bs_icon("opencollective")
+  nav_panel(
+    "Definiciones",
+    card(
+      height = NULL,
+      full_screen = FALSE,
+      accordion_panel(
+        title = "Población potencial",
+        textOutput("defpp_ac")
+      ),
+      accordion_panel(
+        title = "Población objetivo",
+        textOutput("defpo_ac")
+      ),
+      accordion_panel(
+        title = "Población atendida",
+        textOutput("defpa_ac")
+      )
+    )
   ),
-  value_box(
-    title = "Variación del presupuesto", 
-    value = scales::unit_format(unit = "%")(6.0), 
-    showcase = bsicons::bs_icon("plus-slash-minus")
+  nav_panel(
+    shiny::icon("circle-info"),
+    markdown("Información")
   )
 )
+
+## Presupuesto ####
+presupuesto_card <- card(
+  card_header(
+    class = "bg-dark",
+    "Presupuesto"),
+  height = NULL,
+  full_screen = FALSE,
+  plotOutput("graf_pres")
+    )
+
+# Datos generales ####
+
+
+## Funciones ####
+
+ramo <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(ramo) %>% 
+    unique() %>% 
+    sort()
+}
+
+modcve <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(modcve) %>% 
+    unique() %>% 
+    sort()
+}
+
+nombre <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(nombre) %>% 
+    unique() %>% 
+    sort()
+}
+
+ainicio <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(inicio) %>% 
+    unique() %>% 
+    sort()
+}
+
+descripcion <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(descripcion) %>% 
+    unique() %>% 
+    sort()
+}
+
+derecho <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(derecho_directo) %>% 
+    unique() %>% 
+    sort()
+}
+
+etapav <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(etapa_vida) %>% 
+    unique() %>% 
+    sort()
+}
+
+gpoatn <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(gpo_atencion) %>% 
+    unique() %>% 
+    sort()
+}
+
+apoyos <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(nombre) %>% 
+    unique() %>% 
+    sort()
+}
+
+normatividad <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(normatividad) %>% 
+    unique() %>% 
+    sort()
+}
+
+norma_v <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(normatividad_v) %>% 
+    unique() %>% 
+    sort()
+}
+
+fin <- function(anio_sel, ramo_sel, pp_sel){
+  resultado <- inv_fed %>%
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(fin) %>% 
+    unique() %>% 
+    sort()
+  if(length(resultado) == 0) {
+    resultado <- "Información no disponible"
+  }
+  
+  return(resultado)
+}
+
+proposito <- function(anio_sel, ramo_sel, pp_sel){
+  resultado <- inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(proposito) %>% 
+    unique() %>% 
+    sort()
+  
+  if(length(resultado) == 0) {
+    resultado <- "Información no disponible"
+  }
+  
+  return(resultado)
+}
+
+
+componentes <- function(anio_sel, ramo_sel, pp_sel){
+  resultado <- inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(componente) %>% 
+    unique() %>% 
+    sort()
+  
+  if(length(resultado) == 0) {
+    resultado <- "Información no disponible"
+  }
+  
+  return(resultado)
+}
+
+actividades <- function(anio_sel, ramo_sel, pp_sel){
+  resultado <- inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(actividad) %>% 
+    unique() %>% 
+    sort()
+  
+  if(length(resultado) == 0) {
+    resultado <- "Información no disponible"
+  }
+  
+  return(resultado)
+}
+
+gen_mir <- function(fin, proposito, componentes, actividades) {
+  
+  tabla_mir <- data.frame(
+    Nivel = c("Fin", "Propósito", "Componentes", "Actividades"),
+    Objetivo = c(fin, proposito, componentes, actividades),
+    stringsAsFactors = FALSE 
+  )
+  
+  return(tabla_mir)
+}
+
+anio_eval <- function(anio_sel, ramo_sel, pp_sel){
+  id <- evaluaciones %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(id_if) %>% 
+    unique() %>% 
+    sort()
+  
+  if (length(id) > 0) {
+    id <- id[1]
+  } else {
+    return("Información no disponible")
+  }
+  
+  resultado <- evaluaciones %>% 
+    filter(id_if == id) %>% 
+    pull(ciclo) %>% 
+    sort()
+  
+  if(length(resultado) == 0) {
+    resultado <- "Información no disponible"
+  }
+  
+  return(resultado)
+}
+
+titulo_eval <- function(anio_sel, ramo_sel, pp_sel){
+  id <- evaluaciones %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(id_if) %>% 
+    unique() %>% 
+    sort()
+  
+  if (length(id) > 0) {
+    id <- id[1]
+  } else {
+    return("Información no disponible")
+  }
+  
+  resultado <- evaluaciones %>% 
+    filter(id_if == id) %>% 
+    pull(titulo) %>% 
+    sort()
+  
+  if(length(resultado) == 0) {
+    resultado <- "Información no disponible"
+  }
+  
+  return(resultado)
+}
+
+vinculo_eval <- function(anio_sel, ramo_sel, pp_sel){
+  id <- evaluaciones %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(id_if) %>% 
+    unique() %>% 
+    sort()
+  
+  if (length(id) > 0) {
+    id <- id[1]
+  } else {
+    return("Información no disponible")
+  }
+  
+  resultado <- evaluaciones %>% 
+    filter(id_if == id) %>% 
+    pull(vinculo) %>% 
+    sort()
+  
+  if(length(resultado) == 0) {
+    resultado <- "Información no disponible"
+  }
+  
+  return(resultado)
+}
+
+gen_eval <- function(anio_eval, titulo_eval, vinculo_eval) {
+  
+  tabla_eval <- data.frame(
+    Año = anio_eval,
+    'Título de la evaluación' = titulo_eval,
+    'Vínculo a la evaluación' = vinculo_eval,
+    stringsAsFactors = FALSE 
+  )
+  
+  return(tabla_eval)
+}
+
+# Poblaciones ####
+
+## Value boxes ####
+### Cobertura con gráfico y valor del año en curso
+### Eficacia con gráfico y valor del año en curso
+
+## Funciones ####
+unidad_pp <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(pp_unidad) %>% 
+    unique() %>% 
+    sort()
+}
+
+def_pp <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(pp_definicion) %>% 
+    unique() %>% 
+    sort()
+}
+
+unidad_po <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(po_unidad) %>% 
+    unique() %>% 
+    sort()
+}
+
+def_po <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(po_definicion) %>% 
+    unique() %>% 
+    sort()
+}
+
+unidad_pa <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(pa_unidad) %>% 
+    unique() %>% 
+    sort()
+}
+
+def_pa <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(pa_definicion) %>% 
+    unique() %>% 
+    sort()
+}
+
+cobertura <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(cobertura) %>% 
+    unique() %>% 
+    sort()
+}
+
+eficiencia <- function(anio_sel, ramo_sel, pp_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>% 
+    pull(eficiencia) %>% 
+    unique() %>% 
+    sort()
+}
+
+
+grafica_poblaciones <- function(inv_fed, ciclo_sel, ramo_sel, pp_sel) {
+  id <- inv_fed %>%
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>%
+    pull(id_if) %>%
+    unique()
+  
+  if(length(id) == 0) {
+    return(NULL)
+  }
+
+  df_filtrado <- inv_fed %>%
+    filter(id_if %in% id) %>%
+    select(ciclo, pp_cantidad, po_cantidad, pa_cantidad) %>%
+    gather(key = "variable", value = "cantidad", pp_cantidad, po_cantidad, pa_cantidad)
+  
+  ggplot(df_filtrado, aes(x = ciclo, y = cantidad, fill = variable)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    scale_fill_manual(values = c("pp_cantidad" = "#1f77b4", "po_cantidad" = "#ff7f0e", "pa_cantidad" = "#2ca02c")) +
+    labs(title = paste("Serie histórica de poblaciones de", pp_sel),
+         x = "Ciclo",
+         y = "Cantidad",
+         fill = "Variable") +
+    theme_minimal()
+}
+
+# Gráficos de cobertura y eficiencia
+
+
+# Presupuesto ####
+grafica_presupuesto <- function(inv_fed, ciclo_sel, ramo_sel, pp_sel) {
+  id <- inv_fed %>%
+    filter(ciclo == anio_sel, ramo == ramo_sel, nombre == pp_sel) %>%
+    pull(id_if) %>%
+    unique()
+  
+  if(length(id) == 0) {
+    return(NULL)
+  }
+  
+  df_filtrado <- inv_fed %>%
+    filter(id_if %in% id) %>%
+    select(ciclo, p_aprobado, p_ejercido) %>%
+    gather(key = "variable", value = "cantidad", p_aprobado, p_ejercido)
+  
+  ggplot(df_filtrado, aes(x = ciclo, y = cantidad, fill = variable)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    scale_fill_manual(values = c("p_aprobado" = "#1f77b4", "p_ejercido" = "#ff7f0e")) +
+    labs(title = paste("Serie histórica de presupuesto de", pp_sel),
+         x = "Ciclo",
+         y = "Presupuesto",
+         fill = "Variable") +
+    theme_minimal()
+}
+
+ejercido_ciclo <- function(anio_sel){
+  inv_fed %>% 
+    filter(ciclo == anio_sel) %>% 
+    pull(p_ejercido) %>% 
+    sum(na.rm = T)
+}
+## Tarjetas y value boxes####
+### Tarjeta con porcentaje del presupuesto social
+### Value box con gráfico de variaciones en el presupuesto y valor del año en curso vs el anterior
+
+## Gráficos ####
+### Mapa a nivel federal con base de subsidios por municipio
+### Gráfico de líneas con presupuesto aprobado vs ejercido con botón para deflactor
--- inventario_coneval/app.R
+++ inventario_coneval/app.R
@@ -2,100 +2,174 @@ library(shiny)
 library(bslib)
 library(bsicons)
 
-# source("contenido_app")
-
+# source("contenido_app.R")
 
 # UI ####
 ui <- page_sidebar(
-  theme = bslib::bs_theme(bootswatch = "minty"),
-  title = tags$div(
-    style = "display: flex; text-align: center; font-weight: bold; font-size: 36px; padding-right:20px; justify-content: center",
-    "Programas y acciones federales de desarrollo social (2009-2014)",
-    tags$img(src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCwyJ5FDG3rlTp-n_qX9TB603Adrk8Od14w&s", 
-             height = "100px", 
-             style = "display: flex; align-items: right")
-    ), 
-  sidebar = sidebar(
-      radioButtons("dist", "Distribución",
-                   c("Normal"="norm",
-                     "Uniform"="unif",
-                     "Log-Normal"="lnorm",
-                     "Exponential"="exp")),
-    br(),
-    sliderInput("n", 
-                "Observaciones", 
-                value = 100,
-                min = 1, 
-                max = 1000)
+  theme = bslib::bs_theme(bootswatch = "flatly"),
+  title =  fluidRow(
+    column(12,
+           tags$img(src = "https://upload.wikimedia.org/wikipedia/commons/5/5b/Logo_CONEVAL.svg", 
+                    style = "height:10vh; max-height:10vh;"),
+           column(12, 
+                  HTML("<h1>Programas y acciones federales de desarrollo social</h1>"),
+                  HTML("<h2>Información histórica 2009-2024</h2>"))
+           )
   ),
-  navset_pill(
-    header = br(),
-    footer = tags$div(
-      style = "font-size: 10px; padding-right:20px",
-      "Curso de visualización de información con R para la evaluación de políticas públicas 2024. Evaluación final.",
-      br(),
-      "Elaborado por Marvin Ivan Trejo Mendez"), 
-  # navset_card_underline(
-    nav_panel("Datos generales",
-              id_cards[[1]],
-              layout_columns(id_vb[[1]],id_vb[[2]], id_vb[[3]]),
-              layout_columns(id_cards[[2]], id_cards[[3]])
-              ), 
-    nav_panel("Poblaciones", plotOutput("población")), 
-    nav_panel("Presupuesto", plotOutput("presupuesto")), 
-    nav_panel("Evolución", plotOutput("evolucion"))
-  )
+  footer = tags$div(style = "font-size: 10px; padding-right:20px",
+                    "Curso de visualización de información con R para la evaluación de políticas públicas 2024. Evaluación final.",
+                    br(),"Elaborado por Marvin Ivan Trejo Mendez"), 
+  sidebar = sidebar(
+    uiOutput("selCiclo"),
+    uiOutput("selRamo"),
+    uiOutput("selPP")),
+  uiOutput("titulo_dg"),
+  uiOutput("subtitulo_dg"),
+  layout_column_wrap(
+    width = 1/3,
+    heights_equal = "row",
+    datosgrales_card, poblacion_card, presupuesto_card) 
+    # layout_column_wrap(
+    #   width = 1,
+    #   heights_equal = "row",
+    #   poblacion_card, presupuesto_card))
 )
+
 
 # Server ####
 server <- function(input, output, session) {
-  d <- reactive({
-    dist <- switch(input$dist, 
-                   norm = rnorm,
-                   unif = runif,
-                   lnorm = rlnorm, 
-                   exp = rexp, 
-                   rnorm)
-    dist(input$n)
+
+# Controles ####
+  
+  output$selCiclo <- renderUI({
+    selectizeInput(inputId = "selCiclo",
+                label = "Seleccione un año:",
+                choices = c(2009:2022), 
+                selected = 2022)
   })
   
+  output$selRamo <- renderUI({
+    req(input$selCiclo)
+    selectizeInput(inputId = "selRamo",
+                label = "Seleccione la dependencia:",
+                choices = ramos_disp(input$selCiclo))
+  })
+  
+  output$selPP <- renderUI({
+    req(input$selCiclo, input$selRamo)
+    selectInput(inputId = "selPP",
+                label = "Seleccione el programa:",
+                choices = pp_disp(input$selCiclo, input$selRamo))
+  })
+    
 # Datos generales ####  
-  output$descripcion <- renderText({
-    "Descripción del programa"
-    })
-  output$normatividad <- renderText({
-    "Normatividad del programa"
-  })
-  output$apoyos <- renderText({
-    "Apoyos que entrega el programa"
+  output$titulo_dg <- renderUI({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    h2(paste(modcve(input$selCiclo, input$selRamo,input$selPP), 
+             " ", 
+             nombre(input$selCiclo, input$selRamo,input$selPP)),
+      style = "color: #2c3e50; font-weight: bold; text-align: center;")
   })
   
+  output$subtitulo_dg <- renderUI({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    h4(ramo(input$selCiclo, input$selRamo,input$selPP),
+       style = "color: #A9A9A9; font-weight: bold; text-align: center;")
+  })
+
+  output$ainicio_box<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    ainicio(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$descr_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    descripcion(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$derecho_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    derecho(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$etapav_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    etapav(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$gpoatn_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    gpoatn(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$apoyos_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    apoyos(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$tabla_mir<-renderTable({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    
+    mir_f = fin(input$selCiclo, input$selRamo,input$selPP)
+    mir_p = proposito(input$selCiclo, input$selRamo,input$selPP)
+    mir_c = componentes(input$selCiclo, input$selRamo,input$selPP)
+    mir_a = actividades(input$selCiclo, input$selRamo,input$selPP)
+    
+    gen_mir(mir_f, mir_p, mir_c, mir_a)
+  })
+  
+  output$tabla_eval<-renderTable({
+    req(input$selCiclo, input$selRamo,input$selPP)
+
+    eval_anio = anio_eval(input$selCiclo, input$selRamo,input$selPP)
+    eval_titulo = titulo_eval(input$selCiclo, input$selRamo,input$selPP)
+    eval_enlace = vinculo_eval(input$selCiclo, input$selRamo,input$selPP)
+
+    gen_eval(eval_anio, eval_titulo, eval_enlace)
+  })
+
+  # normatividad
+  # norma_v
+# Poblaciones ####
+  output$umpp_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    unidad_pp(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$defpp_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    def_pp(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$umpo_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    unidad_po(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$defpo_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    def_po(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$umpa_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    unidad_pa(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$defpa_ac<-renderText({
+    req(input$selCiclo, input$selRamo,input$selPP)
+    def_pa(input$selCiclo, input$selRamo,input$selPP)
+  })
+  
+  output$graf_pob <- renderPlot({
+    req(input$selCiclo, input$selRamo, input$selPP)
+    grafica_poblaciones(inv_fed, input$selCiclo, input$selRamo, input$selPP)
+  })
+
 # Presupuesto ####  
-  output$presupuesto <- renderPlot({
-    dist <- input$dist
-    n <- input$n
-    
-    hist(d(),
-         main = paste("r", dist, "(", n, ")", sep = ""),
-         col = "#007bc2", border = "white")
-  })
-  
-  output$evolucion <- renderPlot({
-    dist <- input$dist
-    n <- input$n
-    
-    hist(d(),
-         main = paste("r", dist, "(", n, ")", sep = ""),
-         col = "#007bc2", border = "white")
-  })
-  
-  output$poblacion <- renderPlot({
-    dist <- input$dist
-    n <- input$n
-    
-    hist(d(),
-         main = paste("r", dist, "(", n, ")", sep = ""),
-         col = "#007bc2", border = "white")
+  output$graf_pres <- renderPlot({
+    req(input$selCiclo, input$selRamo, input$selPP)
+    grafica_presupuesto(inv_fed, input$selCiclo, input$selRamo, input$selPP)
   })
 }
 
